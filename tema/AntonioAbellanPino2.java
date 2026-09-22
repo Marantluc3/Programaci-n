d46b701b54cc95011df1379a70bd9a2f0de174d0
Marantluc3 <antonioabellanpino@gmail.com>
2026-09-22 13:42:33 +0200

    Add files via upload

---
+++ tema/AntonioAbellanPino2.java
@@ -0,0 +1,6 @@
+// Este programa imprime Antonio Abellan Pino! 
+public class AntonioAbellanPino2 {	
+  public static void main(String[] args) { 
+    System.out.println("Antonio Abellan Pino vivo en C/Madreselva 11B, El Rincón y mi número de teléfono es 611678602");
+  }
+}
