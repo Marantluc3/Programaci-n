9808ac67d3950bdc711522815e1b1a97b5f4593d
Marantluc3 <antonioabellanpino@gmail.com>
2026-09-22 14:27:20 +0200

    Add files via upload

---
+++ tema/Englishletters.java
@@ -0,0 +1,14 @@
+public class Englishletters {	
+  public static void main(String[] args) { 
+    System.out.println("hello \t hola");
+    System.out.println("wednesday \t miércoles");
+    System.out.println("football \t fútbol");
+    System.out.println("download \t descargar");
+    System.out.println("search \t buscar");
+    System.out.println("house \t casa");
+    System.out.println("edit \t editar");
+    System.out.println("stars \t estrellas");
+    System.out.println("build \t construir");
+    System.out.println("pictures \t fotografías");
+  }
+}
