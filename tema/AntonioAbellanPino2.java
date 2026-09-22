09c696265997bbbbbc50a2dbc7a3cb2b67eb86a3
Marantluc3 <antonioabellanpino@gmail.com>
2026-09-22 14:12:57 +0200

    Update AntonioAbellanPino2.java

--- tema/AntonioAbellanPino2.java
+++ tema/AntonioAbellanPino2.java
@@ -1,6 +1,6 @@
 // Este programa imprime Antonio Abellan Pino! 
 public class AntonioAbellanPino2 {	
   public static void main(String[] args) { 
-    System.out.println("Antonio Abellan Pino vivo en C/Madreselva 11B, El Rincón y mi número de teléfono es 611678602");
+    System.out.println("Antonio Abellan Pino vivo en C/Castellón \n y mi número de teléfono es 635 24 48 93");
   }
 }
