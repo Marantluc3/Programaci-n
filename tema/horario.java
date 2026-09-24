747426a4a0e3200d7ee0ba542889fa9582bf1107
Marantluc3 <antonioabellanpino@gmail.com>
2026-09-24 08:47:13 +0200

    Add files via upload

--- tema/horario.java
+++ tema/horario.java
@@ -1,12 +1,20 @@
-public class horario {
+public class horario {	
   public static void main(String[] args) { 
-    System.out.println("             lunes   martes  miercoles jueves   viernes");
+    System.out.println("\t\t lunes martes miercoles jueves viernes");
     System.out.println("-------------------------------------------------------------");
     System.out.println("8:00-9:00 \t LMDAW \t BDDAW \t SIDAW \t PRDAW \t EDDAW");
+    System.out.println("-------------------------------------------------------------");
     System.out.println("9:00-10:00 \t LMDAW \t BDDAW \t SIDAW \t PRDAW \t BDDAW");
+    System.out.println("-------------------------------------------------------------");
     System.out.println("10:00-11:00 \t SIDAW \t SOS \t ITEC \t LMDAW \t BDDAW");
+    System.out.println("-------------------------------------------------------------");
+    System.out.println("11:00-11:30 \trecreo\trecreo\trecreo\trecreo\trecreo");
+    System.out.println("-------------------------------------------------------------");
     System.out.println("11:30-12:30 \t SIDAW \t EDDAW \t ITEC \t SIDAW \t ITEC");
+    System.out.println("-------------------------------------------------------------");
     System.out.println("12:30-13:30 \t PRDAW \t PRDAW \t DIG \t BDDAW \t PRDAW");
+    System.out.println("-------------------------------------------------------------");
     System.out.println("13:30-14:30 \t PRDAW \t PRDAW \t EDDAW \t BDDAW \t PRDAW");
+
     }
 }
